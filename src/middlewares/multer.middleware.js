00dfba212efa02010639f1edf8ef57da170d828f
pras75299 <singh.prashantking@gmail.com>
2024-04-06 00:04:02 +0530

    resolve temp folder location error-no such file or directory

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,8 +1,16 @@
 import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
 
+// Then use it to derive the __dirname
+const __dirname = path.dirname(__filename);
+
+// Resolve an absolute path to your temp directory
+const tempDirectory = path.resolve(__dirname, "../../public/temp");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "../../public/temp");
+    cb(null, tempDirectory);
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
