896287a1579a7962321503cbd781fb15fb2b788d
pras75299 <singh.prashantking@gmail.com>
2025-01-21 22:34:28 +0530

    fix logout user error

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -141,4 +141,8 @@ const loginUser = asyncHandler(async (req, res) => {
     );
 });
 
-export { registerUser, loginUser };
+const logoutUser = asyncHandler(async (req, res) => {
+  User.findByIdAndUpdate(req.user._id, { $set: { refreshToken: undefined } });
+});
+
+export { registerUser, loginUser, logoutUser };
